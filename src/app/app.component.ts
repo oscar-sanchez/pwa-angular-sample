4defef2225149485c36824d3194d72561563fad8
oscar-sanchez <oscar.sanchez.iglesias@haventec.com>
2018-12-30 00:24:23 +1100

    Add HttpClientModel, a service and a basic html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ApiService, Item} from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa-angular-sample';
+  items:  Array<Item>;
+  constructor(private  apiService:  ApiService){
+  }
+
+  ngOnInit(){
+    this.fetchData();
+  }
+
+  fetchData(){
+    this.apiService.fetch().subscribe((data:  Array<Item>)=>{
+      console.log(data);
+      this.items  =  data;
+    }, (err)=>{
+      console.log(err);
+    });
+  }
 }
